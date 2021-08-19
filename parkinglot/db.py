8b475345ab721ec6ca958f694b79b6e77f9632bc
Gil Chen <Gtorrent12@gmail.com>
2021-08-19 23:07:30 +0300

    Updating db.py and interface.py

--- parkinglot/db.py
+++ parkinglot/db.py
@@ -2,44 +2,21 @@ import psycopg2 as pg2
 
 from utils import logger, now
 
-# Establish first connection with default username and password
-try:
-    conn = pg2.connect(user='postgres', password='password')
-    conn.autocommit = True
-    cur = conn.cursor()
-
-except Exception as e:
-    logger.fatal(e, exc_info=True)
-    raise Exception(
-        "It seems like you have some problem with your database, please proceed reading the log for farther understading")
-
-finally:
-    logger.info(
-        "first connection to DB ended successfully status: Done!")
+# Global variables
+conn = None
+cur = None
 
 
 class PostDB:
+    def __init__(self, user: str, password: str) -> None:
+        global conn
+        global cur
 
-    @staticmethod
-    def create_database(name: str) -> None:
-        """:description: Creating a database if not exist"""
-
+        # Establish first connection with default username and password
         try:
-            # Looking if database 'parkinglot' is not exist
-            cur.execute('SELECT datname FROM pg_database;')
-            if name.lower() not in sum(cur.fetchall(), ()):
-                cur.execute(
-                    f"""
-                    CREATE DATABASE {name}
-                    WITH
-                        ENCODING = 'UTF8'
-                        CONNECTION LIMIT = 100
-                        ALLOW_CONNECTIONS = true;
-                    """)
-                logger.info(
-                    f'Database {name} was created successfully!')
-            else:
-                logger.info(f'Database {name} already exist')
+            conn = pg2.connect(user=user, password=password)
+            conn.autocommit = True
+            cur = conn.cursor()
 
         except Exception as e:
             logger.fatal(e, exc_info=True)
@@ -47,7 +24,37 @@ class PostDB:
                 'It seems like you have some problem with your database, please proceed reading the log for farther understading')
 
         finally:
-            logger.info(f'create_database with value {name = } status: Done!')
+            logger.info(
+                'First connection to DB ended successfully status: Done!')
+
+    @staticmethod
+    def create_database(database: str) -> None:
+        """:description: Creating a database if not exist"""
+
+        try:
+            # Looking if database 'parkinglot' is not exist
+            cur.execute('SELECT datname FROM pg_database;')
+            if database.lower() not in sum(cur.fetchall(), ()):
+                cur.execute(
+                    f"""
+                    CREATE DATABASE {database}
+                    WITH
+                        ENCODING = 'UTF8'
+                        CONNECTION LIMIT = 100
+                        ALLOW_CONNECTIONS = true;
+                    """)
+                logger.info(
+                    f'Database {database} was created successfully!')
+            else:
+                logger.info(f'Database {database} already exist')
+
+        except Exception as e:
+            logger.fatal(e, exc_info=True)
+            raise Exception(
+                'It seems like you have some problem with your database, please proceed reading the log for farther understading')
+
+        finally:
+            logger.info(f'create_database with value {database = } status: Done!')
 
     @staticmethod
     def create_table(table: str) -> None:
--- parkinglot/parkinglot_interface.py
+++ parkinglot/parkinglot_interface.py
@@ -4,10 +4,10 @@ from ocr_validator import OcrValidator
 
 class ParkingLot():
     def __init__(self, user: str = 'postgres', password: str = 'password', database: str = 'parkinglot', table: str = 'entrances') -> None:
-        db = PostDB
-        db.create_database(database)
-        db.switch_connection(user, password, database)
-        db.create_table(table)
+        db = PostDB(user=user, password=password)
+        db.create_database(database=database)
+        db.switch_connection(user=user, password=password, database=database)
+        db.create_table(table=table)
 
     @staticmethod
     def check(img: str) -> str:
